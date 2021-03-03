0cb06ea1ed1a8308fa2239ec32d6351855d55fc1
Cesar <cesar@cssguy4hire.com>
2021-03-03 21:03:13 +0200

    02 - Adds TypeScript, linters, SASS support, base Styles, typography, Redux, removes CRA assets

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Component from './components/Component/Component';
+
+import './styles/App.scss';
+
+const App = () => {
+  return (
+    <>
+      <h1>Ejected CRA Boilerplate</h1>
+      <Component text="Hello cruel world!" />
+    </>
+  );
+};
+
+export default App;
